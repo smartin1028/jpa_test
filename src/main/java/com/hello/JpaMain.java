278eb7e140badfe275ede59daf5d2a3a42f58c90
dwsamba <leedw3232@gmail.com>
2022-04-26 14:58:56 +0900

    jpa test

---
+++ src/main/java/com/hello/JpaMain.java
@@ -0,0 +1,27 @@
+package com.hello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		Member member = new Member(1L, "Hello");
+
+		em.persist(member);
+
+		tx.commit();
+
+		em.close();
+		emf.close();
+
+
+	}
+}
